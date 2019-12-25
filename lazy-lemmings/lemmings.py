addcfabdd5d443fa71b1ad8cad102f09c50f5d66
MWQ <michelle.w.qu@gmail.com>
2019-12-25 20:34:53 +0000

    More

--- lazy-lemmings/lemmings.py
+++ lazy-lemmings/lemmings.py
@@ -37,17 +37,21 @@ Find the farthest any single lemming needs to travel for food.
 def furthest(num_holes, cafes):
     """Find longest distance between a hole and a cafe."""
 
-    max_distance = 0
-    lemming_distance = None
-    for lemming in range(num_holes):
-        for cafe in cafes:
-            to_cafe_distance = abs(lemming - cafe)
-            if lemming_distance == None or lemming_distance > to_cafe_distance:
-                lemming_distance = to_cafe_distance
-        if lemming_distance > max_distance:
-            max_distance = lemming_distance
-        lemming_distance = None
-    return max_distance
+    # max_distance = 0
+    # lemming_distance = None
+    # for lemming in range(num_holes):
+    #     for cafe in cafes:
+    #         to_cafe_distance = abs(lemming - cafe)
+    #         if lemming_distance == None or lemming_distance > to_cafe_distance:
+    #             lemming_distance = to_cafe_distance
+    #     if lemming_distance > max_distance:
+    #         max_distance = lemming_distance
+    #     lemming_distance = None
+    # return max_distance
+
+
+    # BINARY
+    
 
 
 def furthest_optimized(num_holes, cafes):
@@ -74,6 +78,25 @@ def furthest_optimized(num_holes, cafes):
         if lemming_distance > max_distance:
             max_distance = lemming_distance
     return max_distance
+    # def furthest_best(num_holes, cafes):
+    # """Find longest distance between a hole and a cafe.
+
+    # This solution looks at the cafes, finding the two that are furthest
+    # apart, and then determining which lemming hole would have to travel
+    # the furthest between them.
+    # """
+
+    # # Start with the distance from first hole to the first cafe, and from
+    # # the last hole to the last cafe:
+
+    # distances = [cafes[0],
+    #              num_holes - cafes[-1] - 1]
+
+    # for i in range(1, len(cafes)):
+    #     # between cafes: distance is half the distance to leftward cafe
+    #     distances.append((cafes[i] - cafes[i - 1]) // 2)
+
+    # return max(distances)
 
 if __name__ == '__main__':
     import doctest
